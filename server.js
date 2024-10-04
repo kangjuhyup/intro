b4acdb537c1a2fca3b270cf85469508fc39ad97c
kangjuhyup <fog0510@gmail.com>
2024-10-04 13:27:29 +0900

    chore : es module

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import * as path from "path";
 const app = express();
 
 const buildPath = path.join(__dirname, "dist");
