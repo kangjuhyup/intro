723274022b1d01b7900b3199ac88511dd69b7888
kangjuhyup <107104811+kangjuhyup@users.noreply.github.com>
2024-10-07 13:10:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const buildPath = path.join(__dirname, "dist");
-app.use(express.static(buildPath));
 
-app.get("*", (req, res) => {
+const globalPrefix = '/intro-page';
+
+app.use(globalPrefix, express.static(buildPath));
+
+app.get(`${globalPrefix}/*`, (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
 
-const globalPrefix = '/intro-page';
-app.use(globalPrefix);
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}${globalPrefix}`);
 });
